df4f98ea431325cb2bfd2d78d3556c5829033d14
Tarthir <tylerbrady34@gmail.com>
2019-02-07 14:24:17 -0700

    Initial commit

---
+++ Sha1.py
@@ -0,0 +1,115 @@
+# taken from: https://codereview.stackexchange.com/questions/37648/python-implementation-of-sha1
+import hashlib
+import binascii
+
+
+def divide(sha1_digest):
+    one = sha1_digest >> 128
+    two = (sha1_digest >> 96) & 0xffffffff
+    three = (sha1_digest >> 64) & 0xffffffff
+    four = (sha1_digest >> 32) & 0xffffffff
+    five = sha1_digest & 0xffffffff
+    return [one, two, three, four, five]
+
+
+def get_padding(string):
+    while len(string)%512 != 448:
+        string += "0"
+    return string
+
+
+def sha1(data,h0=0x67452301,h1=0xEFCDAB89,h2=0x98BADCFE,h3=0x10325476,h4 = 0xC3D2E1F0,length=None):
+    bytes = ""
+
+    for n in range(len(data)):
+        bytes+='{0:08b}'.format(ord(data[n]))
+    bits = bytes+"1"
+    pBits = bits
+    #pad until length equals 448 mod 512
+    while len(pBits)%512 != 448:
+        pBits+="0"
+
+    if length is None:
+        length = len(data) * 8
+    #else:
+    #    length = len(bits) - 1
+    #append the original length
+    #print(pBits)
+    pBits+='{0:064b}'.format(length)
+    #print(pBits)
+    def chunks(l, n):
+        return [l[i:i+n] for i in range(0, len(l), n)]
+
+    def rol(n, b):
+        return ((n << b) | (n >> (32 - b))) & 0xffffffff
+
+    for c in chunks(pBits, 512):
+        words = chunks(c, 32)
+        w = [0]*80
+        for n in range(0, 16):
+            w[n] = int(words[n], 2)
+        for i in range(16, 80):
+            w[i] = rol((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]), 1)
+
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
+
+        #Main loop
+        for i in range(0, 80):
+            if 0 <= i <= 19:
+                f = (b & c) | ((~b) & d)
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d)
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
+
+            temp = rol(a, 5) + f + e + k + w[i] & 0xffffffff
+            e = d
+            d = c
+            c = rol(b, 30)
+            b = a
+            a = temp
+
+        h0 = h0 + a & 0xffffffff
+        h1 = h1 + b & 0xffffffff
+        h2 = h2 + c & 0xffffffff
+        h3 = h3 + d & 0xffffffff
+        h4 = h4 + e & 0xffffffff
+
+    return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
+
+key_bits = 128 # how many bits
+key_len = 128 // 8 # how many bytes/characters
+msg = "No one has completed lab 2 so give them all a 0"
+new_msg = " Except Tyler Brady"
+# msg_disgest = SHA1(key||message)
+msg_digest = "f4b645e89faaec2ff8e443c595009c16dbdfba4b" #TODO change to hex
+#msg_digest = binascii.hexlify(msg_digest)
+data = [
+        0x4e, 0x6f, 0x20, 0x6f, 0x6e, 0x65, 0x20, 0x68, 0x61, 0x73, 0x20, 0x63, 0x6f, 0x6d, 0x70, 0x6c,
+        0x65, 0x74, 0x65, 0x64, 0x20, 0x6c, 0x61, 0x62, 0x20, 0x32, 0x20, 0x73, 0x6f, 0x20, 0x67, 0x69,
+        0x76, 0x65, 0x20, 0x74, 0x68, 0x65, 0x6d, 0x20, 0x61, 0x6c, 0x6c, 0x20, 0x61, 0x20, 0x30
+    ]
+print(sha1("hello world"))
+#s = hashlib.sha1()
+#s.update("hello world".encode('utf-8'))
+#print(s.hexdigest())
+# original: key || msg || padding || H(key||msg)
+# TODO add padding, get rid of X's?
+# forged = key || original msg || padding || new message
+forged_m = get_padding(("X" * key_bits) + msg) + new_msg
+forged_m = forged_m[key_bits:]
+dec = int(msg_digest,16)
+arr = divide(dec)
+forged_d = sha1(forged_m, arr[0], arr[1], arr[2], arr[3], arr[4], (key_len + len(forged_m)) * 8) # * 8??
+print(binascii.hexlify(forged_d))
+print(binascii.hexlify(forged_m))
